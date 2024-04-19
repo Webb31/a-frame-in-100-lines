0af98bd329b54102f766178409ee77529471c1d6
Webb31 <106362447+Webb31@users.noreply.github.com>
2024-04-19 17:56:12 -0400

    Change post image

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -47,7 +47,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+        src: 'https://zd56xv.csb.app/Base_Warpcast_files/CryptoMakesMoneyFasterWhyCryptowithBrianArmstrong.gif',
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
       state: {
