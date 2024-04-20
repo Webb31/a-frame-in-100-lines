d590d16cb950d1daaf00a096393fa82e9952bdc0
Webb31 <106362447+Webb31@users.noreply.github.com>
2024-04-20 12:34:24 -0400

    Skeleton post

---
+++ app/api/post/route.ts
@@ -0,0 +1,26 @@
+import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+    const body: FrameRequest = await req.json();
+    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+    
+    if (!isValid) {
+      return new NextResponse('Message not valid', { status: 500 });
+    }
+
+    return NextResponse.json({ message: 'Hello from the post route' }, { status: 200 });
+
+  }
+  
+  export async function POST(req: NextRequest): Promise<Response> {
+    console.log('POST');
+    return getResponse(req);
+  }
+  
+  export async function GET(req: NextRequest): Promise<Response> {
+    console.log('GET');
+    return getResponse(req);
+  }
+  
+  export const dynamic = 'force-dynamic';
