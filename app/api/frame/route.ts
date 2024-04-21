1805121bb60cb75b64604986789fdac3e0b7a3bc
Webb31 <106362447+Webb31@users.noreply.github.com>
2024-04-20 21:48:13 -0400

    getAttestation

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,33 +1,25 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
-import  { EAS, SchemaEncoder }  from "@ethereum-attestation-service/eas-sdk";
+import { EAS } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from 'ethers';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
-  const easContractAddress = "0x4200000000000000000000000000000000000021";
-  const schemaUID = "0x67f4ef704a08dfb74df8d9191b059ac9515fb5f8ffe83529a342958397fa732c";
-  const eas = new EAS(easContractAddress);
-  // Signer must be an ethers-like signer.
-  await eas.connect("0x596b8eeDe78d360c9484f715919038F3d27fc8Df");
-  // Initialize SchemaEncoder with the schema string
-  const schemaEncoder = new SchemaEncoder("string URL");
-  const encodedData = schemaEncoder.encodeData([
-    { name: "URL", value: "https://youtu.be/LRVJRXMAp2g?si=pCMcY-dI26JN_jHN", type: "string" }
-  ]);
-  const tx = await eas.attest({
-    schema: schemaUID,
-    data: {
-      recipient: "0x0000000000000000000000000000000000000000",
-      expirationTime: 0,
-      revocable: false, // Be aware that if your schema is not revocable, this MUST be false
-      data: encodedData,
-    },
-  });
-  const newAttestationUID = await tx.wait();
-  console.log("New attestation UID:", newAttestationUID);
+  export const EASContractAddress = "0x4200000000000000000000000000000000000021";
+  const eas = new EAS(EASContractAddress);
+  const provider = ethers.getDefaultProvider(
+    "sepolia"
+  );
+  eas.connect(provider);
+
+  const uid = "0xea40915a79a6c699658e225db836ce2cbe09f55aa22e0e4b0e64377bdd78b6ae";
+
+  const attestation = await eas.getAttestation(uid);
+
+  console.log(attestation);
 
   if (isValid) {
     return new NextResponse('Message not valid', { status: 500 });
