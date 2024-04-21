2d2a2026b068bb708eec3eb15613ab8009104582
Webb31 <106362447+Webb31@users.noreply.github.com>
2024-04-20 21:03:17 -0400

    accountAddresses

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,23 +2,22 @@ import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/o
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 import { getEASAttestations } from '@coinbase/onchainkit/identity';
-import { createPublicClient, http } from 'viem';
 import { base } from 'viem/chains';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
-  const address = '0x596b8eeDe78d360c9484f715919038F3d27fc8Df';
-  const attestationsOptions = {
-    schemas: ['0x67f4ef704a08dfb74df8d9191b059ac9515fb5f8ffe83529a342958397fa732c'],
-  };
+//  const address = '0x596b8eeDe78d360c9484f715919038F3d27fc8Df';
+//  const attestationsOptions = {
+//    schemas: ['0x67f4ef704a08dfb74df8d9191b059ac9515fb5f8ffe83529a342958397fa732c'],
+//  };
 
-  const attestations = await getEASAttestations(address, base, attestationsOptions);
-  console.log(attestations);
+//  const attestations = await getEASAttestations(address, base, attestationsOptions);
+//  console.log(attestations);
 
-  if (!isValid) {
-    return new NextResponse('Message not valid', { status: 500 });
-  }
+if (isValid) {
+  accountAddresses = message.interactor.verified_accounts;
+}
   
   return new NextResponse(
     getFrameHtmlResponse({
